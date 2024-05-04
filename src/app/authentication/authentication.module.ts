5064cc54571be7c8bce910c30628f95d60ad1c83
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-04 17:17:06 -0400

    .

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,8 +1,8 @@
+import { AuthenticationService } from './service/authentication.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
 import { SharedModule } from '../shared';
-
 
 @NgModule({
   declarations: [
@@ -14,6 +14,9 @@ import { SharedModule } from '../shared';
   ],
   exports: [
     AuthenticationComponent
+  ],
+  providers: [
+    AuthenticationService
   ]
 })
 export class AuthenticationModule { }
