18613e2f4f272c77fe90d607c12c1e922d415d07
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-05 10:44:57 -0400

    .

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HomeComponent } from './components/home/home.component';
+import { HomeService } from './services/home.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent
+  ],
+  providers: [
+    HomeService
   ]
 })
 export class HomeModule { }
