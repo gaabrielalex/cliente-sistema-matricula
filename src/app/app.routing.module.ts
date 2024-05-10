3697a977aac5553a75a3bc66d9bae69eeb6e350e
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-09 23:26:12 -0400

    .

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/components/home/home.component';
-import { AuthenticationComponent } from './authentication';
+import { AuthenticationComponent } from './authentication/components/authentication/authentication.component';
 
 const routes: Routes = [
   {
