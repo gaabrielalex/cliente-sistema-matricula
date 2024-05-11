50e33013e75376f70a3d85a0c7a1f158c8d1657a
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-11 18:15:23 -0400

    .

--- src/app/authentication/services/authentication.service.ts
+++ src/app/authentication/services/authentication.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Console } from 'console';
+import { tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,10 @@ export class AuthenticationService {
   acess_token: string = '';
 
   //Já sabe que o erro é aqui nesse HttpClient, na hora que faço o uso dele da erro
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
 
   login(email: string, senha: string){
     return this.httpClient.post(
@@ -26,5 +31,31 @@ export class AuthenticationService {
       return true;
     }
     return false;
+  }
+
+  logout(){
+
+    return this.httpClient.post(
+      this.apiUrl + '/logout',
+      {},
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'application/json')
+          .set('x-access-token', localStorage.getItem('Token') ?? ''),
+        observe: 'response',
+      }
+    ).pipe(
+      tap((response: any) => {
+        // Verifique se o status da resposta é 200
+        console.log(response);
+        if (response.status === 200) {
+          // Executar ações desejadas aqui, como redirecionar o usuário ou exibir uma mensagem de sucesso
+          console.log('Logout bem-sucedido');
+
+          localStorage.removeItem('Token');
+          localStorage.removeItem('User');
+          this.router.navigate(['/login']);
+        }
+      }));
   }
 }
--- src/app/default-layout/components/default-layout/default-layout.component.ts
+++ src/app/default-layout/components/default-layout/default-layout.component.ts
@@ -1,5 +1,11 @@
+import { AuthenticationService } from './../../../authentication/services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 import { DefaultLayoutService } from '../../services/default-layout.service';
+import { Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-default-layout',
@@ -8,8 +14,16 @@ import { DefaultLayoutService } from '../../services/default-layout.service';
 })
 export class DefaultLayoutComponent implements OnInit {
   tipoUsuario: string = '';
+  responseMessage: any;
 
-  constructor(private defaultLayoutService: DefaultLayoutService) {
+  constructor(
+    private defaultLayoutService: DefaultLayoutService,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private ngxSerivce: NgxUiLoaderService,
+    private snackBarService: SnackbarService
+  ) {
     const usuarioString = localStorage.getItem('User');
 
     if (usuarioString) {
@@ -27,5 +41,22 @@ export class DefaultLayoutComponent implements OnInit {
   badgevisible = false;
   badgevisibility() {
     this.badgevisible = true;
+  }
+
+  logout() {
+    this.ngxSerivce.start();
+
+    this.authenticationService.logout().subscribe((response: any) => {
+      this.ngxSerivce.stop();
+      this.snackBarService.openSnackBar("Logout efetuado com sucesso", GlobalConstants.success);
+    },(error) => {
+      this.ngxSerivce.stop();
+      if(error.error?.error != null){
+        this.responseMessage = error.error.error;
+      } else {
+        this.responseMessage = GlobalConstants.GenereicErrorMessage;
+      }
+      this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
   }
 }
