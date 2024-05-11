3ef4c9a4517ef87ad78c2d8573c4ba1250f1edeb
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-11 16:06:07 -0400

    .

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: '**',
+        redirectTo: '/home'
       }
     ]
   },
