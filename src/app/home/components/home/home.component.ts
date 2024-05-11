80a3d7f395e198fa0b10205433eacbbcef86ba8a
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-11 17:00:04 -0400

    .

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -8,10 +8,23 @@ import { MatSidenav } from '@angular/material/sidenav';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  userName: string | null = 'fictional person';
 
   constructor(
     private homeService: HomeService
-  ) {}
+  ) {
+    const usuarioString = localStorage.getItem('User');
+
+    if (usuarioString) {
+      // 2. Converte a string JSON de volta para um objeto JavaScript
+      const usuario = JSON.parse(usuarioString);
+
+      // 3. Acessa a propriedade 'nome' do objeto para obter o nome do usuário
+      this.userName = usuario.nome;
+    } else {
+      this.userName = 'Nome não encontrado';
+    }
+  }
 
   ngOnInit(): void {
   }
