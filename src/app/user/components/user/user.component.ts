430654822040875d282c9b237667351f229bf094
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-12 14:10:50 -0400

    .

--- src/app/user/components/user/user.component.ts
+++ src/app/user/components/user/user.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.service';
+import { MatTab } from '@angular/material/tabs';
+import { MatTableDataSource } from '@angular/material/table';
+import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-user',
@@ -8,10 +15,36 @@ import { UserService } from '../../services/user.service';
 })
 export class UserComponent implements OnInit {
   userTitle = 'Usuários';
+  displayedColumns: string[] = ['name', 'email', 'tipo', 'actions'];
+  dataSource: any;
+  responseMessage: any;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    public dialogRef: MatDialog,
+    private ngxSerivce: NgxUiLoaderService,
+    private snackBarService: SnackbarService
+  ) { }
 
   ngOnInit(): void {
+    // this.ngxSerivce.start();
+    // this.tableData();
   }
 
+  // tableData(){
+  //   this.userService.getUsers().subscribe((response: any) => {
+  //     this.ngxSerivce.stop();
+  //     this.dataSource = new MatTableDataSource(response);
+  //   },(error) => {
+  //     this.ngxSerivce.stop();
+  //     console.log(error);
+  //     if(error.error?.error != null){
+  //       this.responseMessage = error.error.error;
+  //     } else {
+  //       this.responseMessage = 'Erro ao buscar os usuários';
+  //     }
+  //     this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+  //   })
+  // }
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  apiUrl: string = environment.apiUrl;
+  acess_token: string = '';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
+
+  addUser(name: string, email: string, senha: string, tipo: 'A' | 'V'){
+    //Apenas usuários do tipo adminstrador e avaliador podem ser cadastrados por meio desse função, o cadastro
+    //de usários do tipo aluno é feito por outra parte do sistema
+    return this.httpClient.post(
+      this.apiUrl + '/user',
+      {name: name, email: email, senha: senha, tipo: tipo},
+      {headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('x-access-token', localStorage.getItem('Token') ?? '')}
+    );
+  }
+
+  // getUsers(){
+  //   return this.httpClient.get(
+  //     this.apiUrl + '/user',
+  //     {headers: new HttpHeaders()
+  //       .set('Content-Type', 'application/json')
+  //       .set('x-access-token', localStorage.getItem('Token') ?? '')}
+  //   );
+  // }
 }
