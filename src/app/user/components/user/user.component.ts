7d31385f3a4f9954d1e1d0f3498abdb3f1c0b1c5
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-13 01:20:25 -0400

    .

--- src/app/user/components/user/user.component.ts
+++ src/app/user/components/user/user.component.ts
@@ -7,6 +7,8 @@ import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.servi
 import { MatTab } from '@angular/material/tabs';
 import { MatTableDataSource } from '@angular/material/table';
 import { GlobalConstants } from 'src/app/shared/global-constants';
+import { CdkTableModule} from '@angular/cdk/table';
+import {DataSource} from '@angular/cdk/table';
 
 @Component({
   selector: 'app-user',
@@ -15,7 +17,7 @@ import { GlobalConstants } from 'src/app/shared/global-constants';
 })
 export class UserComponent implements OnInit {
   userTitle = 'Usuários';
-  displayedColumns: string[] = ['name', 'email', 'tipo', 'actions'];
+  displayedColumns: string[] = ['nome', 'email', 'tipo', 'acoes'];
   dataSource: any;
   responseMessage: any;
 
@@ -28,23 +30,44 @@ export class UserComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.ngxSerivce.start();
-    // this.tableData();
+    this.ngxSerivce.start();
+    this.tableData();
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      return data.email == filter;
+     };
   }
 
-  // tableData(){
-  //   this.userService.getUsers().subscribe((response: any) => {
-  //     this.ngxSerivce.stop();
-  //     this.dataSource = new MatTableDataSource(response);
-  //   },(error) => {
-  //     this.ngxSerivce.stop();
-  //     console.log(error);
-  //     if(error.error?.error != null){
-  //       this.responseMessage = error.error.error;
-  //     } else {
-  //       this.responseMessage = 'Erro ao buscar os usuários';
-  //     }
-  //     this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
-  //   })
-  // }
+  tableData(){
+    this.userService.getUsers().subscribe((response: any) => {
+      this.ngxSerivce.stop();
+      console.log(response);
+      this.dataSource = new MatTableDataSource(response);
+    },(error) => {
+      this.ngxSerivce.stop();
+      console.log(error);
+      if(error.error?.error != null){
+        this.responseMessage = error.error.error;
+      } else {
+        this.responseMessage = 'Erro ao buscar os usuários';
+      }
+      this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
+
+  ////TESTAR TUDO
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handleAddAction(){
+  }
+
+  handleEditAction(values: any) {
+
+  }
+
+  handleDeleteAction(values: any) {
+
+  }
 }
