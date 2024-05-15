3807a61ea105ecae7129338716918ad5e4eed836
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-15 18:55:34 -0400

    .

--- src/app/editais/editais.module.ts
+++ src/app/editais/editais.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { EditaisRoutingModule } from './editais-routing.module';
 import { EditaisComponent } from './components/editais/editais.component';
 import { EditaisService } from './services/editais.service';
-
+import { SharedModule } from '../shared';
+import { MaterialModule } from '../shared/material.module';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -11,6 +17,13 @@ import { EditaisService } from './services/editais.service';
   ],
   imports: [
     CommonModule,
+    EditaisRoutingModule,
+    SharedModule,
+    MaterialModule,
+    MatTableModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   exports: [
     EditaisComponent
