2a466d1e21ab68c4a08268dad81c32993482bc65
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-20 18:09:48 -0400

    .

--- src/app/reset-password/components/reset-password/reset-password.component.ts
+++ src/app/reset-password/components/reset-password/reset-password.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ResetPasswordService } from '../../services/reset-password.service';
+import { UserService } from 'src/app/user/services/user.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,10 +12,26 @@ import { ResetPasswordService } from '../../services/reset-password.service';
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent implements OnInit {
+  resetPasswordForm: any = FormGroup;
+  responseMessage: any;
 
-  constructor(private resetPasswordService: ResetPasswordService) { }
+  constructor(
+    private resetPasswordService: ResetPasswordService,
+    private userService: UserService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private ngxSerivce: NgxUiLoaderService,
+    private snackBarService: SnackbarService
+  ) { }
 
   ngOnInit(): void {
+    this.resetPasswordForm = this.formBuilder.group({
+      senhaAtual: [null, [Validators.required]],
+      novaSenha: [null, [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  handledSubmitResetPassword(){
+
+  }
 }
