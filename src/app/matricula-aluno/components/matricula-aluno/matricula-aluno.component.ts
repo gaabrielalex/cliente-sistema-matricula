ec6d231565d9dcf5c737010c6722af5a5cfc597a
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-20 22:17:20 -0400

    .

--- src/app/matricula-aluno/components/matricula-aluno/matricula-aluno.component.ts
+++ src/app/matricula-aluno/components/matricula-aluno/matricula-aluno.component.ts
@@ -7,10 +7,36 @@ import { MatriculaAlunoService } from '../../services/matricula-aluno.service';
   styleUrls: ['./matricula-aluno.component.css']
 })
 export class MatriculaAlunoComponent implements OnInit {
+  // Status posssíveis
+  // X - Matricula inexistente
+  // I - Matricula Incompleta(documentações ainda não enviadas)
+  // C - Matricula Completa(documentações enviadas)
+  // A - Matricula Aprovada pelo avaliador responsável(documentações aprovadas)
+  // R - Matricula Reprovada pelo avaliador responsável(documentações reprovadas)
+  //Por padrão a matricula é inexistente
+  statusMatricula: String = 'X'
 
   constructor(private matriculaAlunoService: MatriculaAlunoService) { }
 
   ngOnInit(): void {
+    const usuarioString = localStorage.getItem('User');
+    const usuario = JSON.parse(usuarioString || '{}');
+
+    this.matriculaAlunoService.getByEmail(usuario.email).subscribe((response: any) => {
+      if(response.length > 0){
+        console.log("entrou");
+        //Se existir matricula então o status é o status da matricula
+        this.statusMatricula = response[0].status;
+
+      } else {
+        //Se não existir matricula então o status é X
+        this.statusMatricula = 'X';
+      }
+      console.log("O status da mat´ricula foi definido como: " + this.statusMatricula)
+    },(error) => {
+      console.log("O status da mat´ricula foi definido como: " + this.statusMatricula)
+      console.log(error);
+    })
   }
 
 }
--- src/app/matricula-aluno/services/matricula-aluno.service.ts
+++ src/app/matricula-aluno/services/matricula-aluno.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalConstants } from 'src/app/shared/others/global-constants';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatriculaAlunoService {
+  apiUrl: string = environment.apiUrl;
+  path_route: string = '/matricula';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
+
+  getByEmail(email: string){
+    return this.httpClient.get(
+      this.apiUrl + this.path_route + `/${email}`,
+      GlobalConstants.default_headers_routes
+    );
+  }
+
 }
