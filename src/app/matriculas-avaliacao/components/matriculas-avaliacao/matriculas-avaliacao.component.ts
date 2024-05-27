248c6f00ec7f7a6bfb430872ffd589a8b0e6f406
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-27 02:55:56 -0400

    add page maticula avliacao

--- src/app/matriculas-avaliacao/components/matriculas-avaliacao/matriculas-avaliacao.component.ts
+++ src/app/matriculas-avaliacao/components/matriculas-avaliacao/matriculas-avaliacao.component.ts
@@ -1,5 +1,11 @@
+import { Router } from '@angular/router';
 import { MatriculasAvaliacaoService } from './../../services/matriculas-avaliacao.service';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { GlobalConstants } from 'src/app/shared/others/global-constants';
 
 @Component({
   selector: 'app-matriculas-avaliacao',
@@ -7,10 +13,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./matriculas-avaliacao.component.css']
 })
 export class MatriculasAvaliacaoComponent implements OnInit {
+  displayedColumns: string[] = ['nome', 'email', 'cpf', 'acoes'];
+  dataSource: any;
+  responseMessage: any;
 
-  constructor(private matriculasAvaliacaoService: MatriculasAvaliacaoService) { }
+  constructor(
+    private matriculasAvaliacaoService: MatriculasAvaliacaoService,
+    private router: Router,
+    public dialogRef: MatDialog,
+    private ngxSerivce: NgxUiLoaderService,
+    private snackBarService: SnackbarService
+  ) { }
 
   ngOnInit(): void {
+    this.ngxSerivce.start();
+    this.tableData();
+    this.dataSource.filterPredicate = (data: any, filter: string) => {
+      return data.email == filter;
+    };
   }
 
+  tableData(){
+    const usuarioString = localStorage.getItem('User');
+    const usuario = JSON.parse(usuarioString || '{}');
+    const idAvaliador : number = usuario.id_usuario;
+
+    this.matriculasAvaliacaoService.get(idAvaliador).subscribe((response: any) => {
+      this.ngxSerivce.stop();
+      console.log(response);
+      this.dataSource = new MatTableDataSource(response.matriculas);
+    },(error) => {
+      this.ngxSerivce.stop();
+      console.log(error);
+      if(error.error?.error != null){
+        this.responseMessage = error.error.error;
+      } else {
+        this.responseMessage = 'Erro ao buscar as matrículas para avaliação';
+      }
+      this.snackBarService.openSnackBar(this.responseMessage, GlobalConstants.error);
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  handleAvaliarAction(values: any) {
+    // const dialogConifg = new MatDialogConfig();
+    // dialogConifg.data = {
+    //   action: 'Edit',
+    //   data: values
+    // }
+    // dialogConifg.width = '850px';
+    // const dialogRef = this.dialogRef.open(EditaisDialogAddEditComponent, dialogConifg);
+    // this.router.events.subscribe(() => {
+    //   dialogRef.close();
+    // });
+    // const sub = dialogRef.componentInstance.onEditEdital.subscribe(
+    //   (response) => {
+    //     this.tableData();
+    //   }
+    // );
+  }
 }
--- src/app/matriculas-avaliacao/services/matriculas-avaliacao.service.ts
+++ src/app/matriculas-avaliacao/services/matriculas-avaliacao.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalConstants } from 'src/app/shared/others/global-constants';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatriculasAvaliacaoService {
+  apiUrl: string = environment.apiUrl;
+  path_route: string = '/matriculas-avaliacao';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
+
+  get(idAvaliador: number){
+    return this.httpClient.get(
+      this.apiUrl + this.path_route + `/${idAvaliador}`,
+      GlobalConstants.default_headers_routes
+    );
+  }
 }
