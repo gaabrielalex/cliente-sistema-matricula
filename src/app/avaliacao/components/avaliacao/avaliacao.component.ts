6a50cde50bf7ac08938a207115219929f27acb90
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-27 16:44:10 -0400

    .

--- src/app/avaliacao/components/avaliacao/avaliacao.component.ts
+++ src/app/avaliacao/components/avaliacao/avaliacao.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AvaliacaoService } from '../../services/avaliacao.service';
+import { ActivatedRoute } from '@angular/router';
+import { SnackbarService } from 'src/app/shared/services/snackbar/snackbar.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-avaliacao',
@@ -7,10 +10,30 @@ import { AvaliacaoService } from '../../services/avaliacao.service';
   styleUrls: ['./avaliacao.component.css']
 })
 export class AvaliacaoComponent implements OnInit {
+  idMatricula: number = 0;
 
-  constructor(private avaliacaoService: AvaliacaoService) { }
+  constructor(
+    private avaliacaoService: AvaliacaoService,
+    private route: ActivatedRoute,
+    private FormBuilder: FormBuilder,
+    private snackbarService: SnackbarService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const id = params.get('id_matricula');
+      this.idMatricula = id !== null ? +id : 0;
+      if (this.idMatricula !== null) {
+        this.carregarDados(this.idMatricula);
+      } else {
+        console.error('id_matricula is null');
+      }
+    });
+  }
+
+  carregarDados(id: number) {
+    // this.snackbarService.openSnackBar('O ID da matrícula é: ' + id, 'OK');
+
   }
 
 }
