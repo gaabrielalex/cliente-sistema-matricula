35437db2216257cb9cbe05b0dd25fd1755546b98
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-05-27 19:19:07 -0400

    .

--- src/app/avaliacao/services/avaliacao.service.ts
+++ src/app/avaliacao/services/avaliacao.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalConstants } from 'src/app/shared/others/global-constants';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvaliacaoService {
+  apiUrl: string = environment.apiUrl;
+  path_route: string = '/avaliacoes';
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router,
+  ) { }
+
+  getAlunoByIdMatricula(idMatricula: number){
+    return this.httpClient.get(
+     `${this.apiUrl}/matricula/${idMatricula}/aluno`,
+      GlobalConstants.default_headers_routes
+    );
+  }
 }
