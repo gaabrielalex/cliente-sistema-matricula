2fd59f0e36bfbe62dadbca120c93ed82c806da22
gaabrielalex <gabrielalexsilva2@gmail.com>
2024-07-17 22:25:36 -0400

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   favicon: 'favicon.prod.ico',
-  apiUrl: 'https://servidor-sistema-matricula.vercel.app',
-  // apiUrl: 'http://localhost:9020',
+  apiUrl: 'http://localhost:9020',
 };
